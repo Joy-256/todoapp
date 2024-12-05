68b0fc40382d02b71705fd818a650470211917b2
Joy-256 <mbabazi14joy@gmail.com>
2024-12-05 10:47:47 +0300

    todoapp

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,16 @@
+import TodoItem from "./TodoItem";
+import styles from './todolist.module.css';
+
+export default function TodoList({ todos,setTodos }) {
+  const sortedTodos = todos
+   .slice()
+   .sort((a,b) => Number(a.done) - Number(b.done))
+    console.log(sortedTodos)
+    return (
+      <div className={styles.list}>
+        {sortedTodos.map((item)=>(
+        <TodoItem item={item} key={item.name} todos={todos} setTodos={setTodos}/>
+      ))}
+      </div>
+    );
+}
